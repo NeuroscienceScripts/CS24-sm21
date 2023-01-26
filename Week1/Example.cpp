6f159f665d53158bf2ba80c016e53001f83fabf7
NeuroscienceScripts <52755909+NeuroscienceScripts@users.noreply.github.com>
2023-01-26 14:17:24 -0800

    Add files via upload

---
+++ Week1/Example.cpp
@@ -0,0 +1,7 @@
+#include "Example.h" //Can use all declarations/definitions in Example.h header file
+
+//Definition for setNumber (declared in Example.h)
+void Example::setNumber(int first, int second){ // Must state in scope of Example
+    number1 = first;
+    number2 = second;
+}
