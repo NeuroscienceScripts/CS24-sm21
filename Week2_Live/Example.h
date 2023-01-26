47eaff7f02be52937b3242cefcf6cc951fc97582
NeuroscienceScripts <52755909+NeuroscienceScripts@users.noreply.github.com>
2023-01-26 14:19:19 -0800

    Add files via upload

---
+++ Week2_Live/Example.h
@@ -0,0 +1,20 @@
+//
+// Example header file, contains all declarations so files that use Example know
+// what functions are available
+//
+
+#ifndef UNTITLED_EXAMPLE_H
+#define UNTITLED_EXAMPLE_H
+
+class Example{
+private:
+    int number1;
+    int number2;
+public:
+    Example(int first, int second){number1=first; number2=second; }
+    Example(int first){number1=first; number2=10; }
+    void setNumber(int newNumber, int whichNumber);
+    int getNumber(int whichNumber){return whichNumber==1 ? number1 : number2; }
+};
+
+#endif //UNTITLED_EXAMPLE_H
